c7065a7a973f72c9b7ee316ca1637431191fe12b
DeedleFake <deedlefake@users.noreply.github.com>
2025-06-23 19:10:30 -0400

    trunic: add godoc comments

--- renderer.go
+++ renderer.go
@@ -13,6 +13,7 @@ import (
 
 const letterWidthRatio = .6
 
+// Renderer draws Trunic text to an image.
 type Renderer struct {
 	Color      color.Color // Default: color.Black
 	TextHeight float64     // Default: 72
@@ -42,10 +43,18 @@ func (r *Renderer) thickness() float64 {
 	return r.Thickness
 }
 
+// AppendRune is a low-level method that appends pieces of runes to
+// the Renderer's internal buffer. Each call appends a single rune
+// which is drawn by overlapping all of the symbols corresponding to
+// the strings passed. Generally speaking, this is a single vowel and
+// a single consonant.
 func (r *Renderer) AppendRune(ph ...string) {
 	r.ph = append(r.ph, slices.Clone(ph))
 }
 
+// DrawTo draws the Renderer's current state to dst with the top-left
+// corner at (x, y). The coordinates are relative to (0, 0), not to
+// the top-left of dst's bounding box.
 func (r *Renderer) DrawTo(dst draw.Image, x, y float64) {
 	renderer := rasterizer.FromImage(dst, 1, nil)
 
@@ -61,7 +70,10 @@ func (r *Renderer) DrawTo(dst draw.Image, x, y float64) {
 	letterWidth := letterWidthRatio * letterHeight
 
 	offset := dst.Bounds().Canon().Min
-	m := canvas.Identity.Translate(float64(-offset.X), float64(-offset.Y)).Scale(letterWidth/2, letterHeight/6)
+	m := canvas.Identity.
+		Translate(x, y).
+		Translate(float64(-offset.X), float64(-offset.Y)).
+		Scale(letterWidth/2, letterHeight/6)
 
 	for i, ph := range r.ph {
 		if len(ph) == 0 {
@@ -73,15 +85,24 @@ func (r *Renderer) DrawTo(dst draw.Image, x, y float64) {
 			p = p.Join(pathFor(ph))
 		}
 
-		lx := x + float64(i)*letterWidth
+		lx := float64(i) * letterWidth
 		c.DrawPath(lx, 0, p.Transform(m))
 	}
 }
 
+// Bounds returns the minimum bounding box that will contain the
+// result of rendering the Renderer's current state with the top-left
+// corner at (0, 0). Note that this is a minimum that does not take
+// into account the thickness of the lines. Default thickness usually
+// requires about 10 pixels of padding or so to fit the result without
+// anything getting clipped.
 func (r *Renderer) Bounds() image.Rectangle {
 	return image.Rectangle{Max: r.Size()}
 }
 
+// Size returns the size of the image. This is equivalent to
+//
+//	r.Bounds().Size()
 func (r *Renderer) Size() image.Point {
 	height := r.textHeight()
 	width := letterWidthRatio * height
@@ -90,9 +111,4 @@ func (r *Renderer) Size() image.Point {
 		len(r.ph)*int(math.Ceil(width)),
 		int(math.Ceil(height)),
 	)
-}
-
-func t(x, y float64, m canvas.Matrix) (float64, float64) {
-	p := m.Dot(canvas.Point{X: x, Y: y})
-	return p.X, p.Y
 }
